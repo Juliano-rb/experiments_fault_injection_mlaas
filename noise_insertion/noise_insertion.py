3184654dcde11d7a16148b94b789c2fea911251c
juliano-rb <rocha.juliano.b@gmail.com>
2021-08-21 14:18:03 -0300

    adiciona char_swap noise

--- noise_insertion/noise_insertion.py
+++ noise_insertion/noise_insertion.py
@@ -56,8 +56,33 @@ def ocr_aug(text_lists, aug_level=0.3):
 def random_noise(text_lists,aug_level=0.3):
     aug = nac.RandomCharAug(aug_char_p=aug_level,
                     aug_char_max=None,
-                    tokenizer = custom_tokenizer.tokenizer,
-                    reverse_tokenizer=custom_tokenizer.reverse_tokenizer)
+                    tokenizer = tokenizer,
+                    reverse_tokenizer=reverse_tokenizer)
+
+    augmented_texts = []
+
+    for text in text_lists:
+        augmented_text = aug.augment(text, n=1)
+        augmented_texts.append(augmented_text)
+
+    return augmented_texts
+
+def char_swap_noise(text_lists,aug_level=0.3):
+    """
+    Noise insertion by swapping characters.
+
+    Notes:
+        - noise level grows up in even number increments because in each swap two characters are changed
+        - it seems that in some situations it repeats the swap in the same characters, making them go back to what they were before and decreasing the noise level 
+    :param text_lists: list of texts to generate noise in
+    :aug_level: noise level
+    """
+    aug = nac.RandomCharAug(action='swap',
+                    swap_mode='adjacent', # adjacent, middle or random
+                    aug_char_p=aug_level/2, # needs to be divided because nlpaug interpretation of augmentation unit
+                    aug_char_max=None,
+                    tokenizer = tokenizer,
+                    reverse_tokenizer= reverse_tokenizer)
 
     augmented_texts = []
 
