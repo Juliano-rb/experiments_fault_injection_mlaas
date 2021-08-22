1e0e075ee0f1c689492d98131e1b754ca2c8b10a
juliano-rb <rocha.juliano.b@gmail.com>
2021-08-21 21:21:41 -0300

    run_evaluation recebe um array de providers para testar todos e plot_results foi adaptada para funcionar com vários providers

--- fault_injection_text.py
+++ fault_injection_text.py
@@ -14,9 +14,7 @@ from sklearn.metrics import precision_score
 from sklearn.metrics import roc_auc_score
 from utils import preprocessing as pre
 from noise_insertion import noise_insertion
-from datetime import datetime
 from utils import visualization
-from pathlib import Path
 
 def calc_dataset_metrics(y_labels, predicted_labels):
 
@@ -68,68 +66,55 @@ def print_metrics(metrics_dict):
 def run_evaluation(x_dataset, y_labels,
                   noise_levels=[0.1, 0.15, 0.2, 0.25, 0.3],
                   noise_algorithms=[noise_insertion.no_noise, noise_insertion.random_noise, noise_insertion.keyboard_aug, noise_insertion.ocr_aug],
-                  mlaas_provider=providers.naive_classifier):
+                  mlaas_providers=[providers.naive_classifier]):
     results = []
 
-    # Executa apenas uma vez para a opção sem ruído
-    if noise_insertion.no_noise in noise_algorithms:
-        algorithm = noise_insertion.no_noise
-        noise_algorithms.remove(algorithm)
-        level = 0
+    for provider in mlaas_providers:
+        # Executa apenas uma vez para a opção sem ruído
+        if noise_insertion.no_noise in noise_algorithms:
+            algorithm = noise_insertion.no_noise
+            noise_algorithms.remove(algorithm)
+            level = 0
 
-        noised_dataset = generate_noised_dataset(x_dataset,level, algorithm)
-        predicted_labels = get_prediction_results(noised_dataset, mlaas_provider)
-        
-        acc, recall, precision, auc = calc_dataset_metrics(y_labels,predicted_labels)
-
-        result = {'provider':mlaas_provider.__name__,
-                    'noise_algorithm':algorithm.__name__,
-                    'noise_level':0 if algorithm.__name__ == 'no_noise' else level,
-                    'acc':acc, 'recall':recall, 'precision': precision, 'auc': auc
-        }
-        print('#####',result)
-
-        results.append(result)
-        
-        
-
-    # Executa com o restante dos algoritmos
-    for algorithm in noise_algorithms:
-        for level in noise_levels:
             noised_dataset = generate_noised_dataset(x_dataset,level, algorithm)
-            predicted_labels = get_prediction_results(noised_dataset, mlaas_provider)
+            predicted_labels = get_prediction_results(noised_dataset, provider)
             
             acc, recall, precision, auc = calc_dataset_metrics(y_labels,predicted_labels)
 
-            result = {'provider':mlaas_provider.__name__,
-                      'noise_algorithm':algorithm.__name__,
-                      'noise_level':0 if algorithm.__name__ == 'no_noise' else level,
-                      'acc':acc, 'recall':recall, 'precision': precision, 'auc': auc
+            result = {'provider':provider.__name__,
+                        'noise_algorithm':algorithm.__name__,
+                        'noise_level':0 if algorithm.__name__ == 'no_noise' else level,
+                        'acc':acc, 'recall':recall, 'precision': precision, 'auc': auc
             }
             print('#####',result)
 
             results.append(result)
-    
-    now = datetime.now()
-    timestamp = now.strftime("%m-%d-%Y %H_%M_%S")
-    print(len(x_dataset))
-    path = 'outputs/size'+str(len(x_dataset))+'_' + timestamp
-    Path(path).mkdir(parents=True, exist_ok=True)
 
-    filename = path + '/data.json'
+        # Executa com o restante dos algoritmos
+        for algorithm in noise_algorithms:
+            for level in noise_levels:
+                noised_dataset = generate_noised_dataset(x_dataset,level, algorithm)
+                predicted_labels = get_prediction_results(noised_dataset, provider)
+                
+                acc, recall, precision, auc = calc_dataset_metrics(y_labels,predicted_labels)
 
-    f = open(filename, "w")
-    f.write(str(results))
-    f.close()
+                result = {'provider':provider.__name__,
+                        'noise_algorithm':algorithm.__name__,
+                        'noise_level':0 if algorithm.__name__ == 'no_noise' else level,
+                        'acc':acc, 'recall':recall, 'precision': precision, 'auc': auc
+                }
+                print('#####',result)
 
-    visualization.plot_results(results, path)
+                results.append(result)
+
+    visualization.plot_results(results, len(x_dataset))
 
 # Importanto os dados relacionados a classificação de sentimentos em revisão de filmes.
 # Fonte: https://www.kaggle.com/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
 df = pd.read_csv('./imdb_dataset.csv', encoding ='utf-8')
 
 # Selecionando somente uma amostra dos dados
-sample_size = 50
+sample_size = 10
 df = df.groupby('sentiment').apply(lambda x: x.sample(int(sample_size/2)))
 
 # Aplicando função de tratamento do texto nas revisões:
@@ -147,5 +132,5 @@ run_evaluation(
     X, dataset_label,
     noise_levels=[0.1, 0.15, 0.2, 0.25, 0.3],
     # noise_levels=[0.1,  0.15],
-    noise_algorithms=[noise_insertion.no_noise, noise_insertion.random_noise, noise_insertion.keyboard_aug, noise_insertion.ocr_aug],
-    mlaas_provider=providers.google)
+    noise_algorithms=[noise_insertion.no_noise, noise_insertion.random_noise, noise_insertion.keyboard_aug, noise_insertion.ocr_aug, noise_insertion.char_swap_noise],
+    mlaas_providers=[providers.azure, providers.naive_classifier])
