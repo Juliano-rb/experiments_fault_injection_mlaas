fd71a7d01af1bab854549d6e4bf509a64aed717a
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2021-10-02 19:00:40 -0300

    implementa uma fila que controla a velocidade com que as requisições são realizadas
    ajusta google provider para utilizar a fila

--- mlaas_providers/google_sentiment_analysis.py
+++ mlaas_providers/google_sentiment_analysis.py
@@ -1,18 +1,15 @@
 from google.cloud import language_v1
-
-from time import sleep
-
 from mlaas_providers.sentiment_analysis import SentimentAnalysis
-
 import queue
-import threading, time, random
+from utils.requestQueue import RequestQueue
 
 text_type = language_v1.Document.Type.PLAIN_TEXT
 
-def call_google_sentiment(client, review, index, result_queue):
+def call_google_sentiment(review, client, result_queue):
     document = language_v1.Document(content=review, type_=text_type, language="EN")
     result = client.analyze_sentiment(request={'document': document})
-    result_queue.put({'sentiment': 'positive' if result.document_sentiment.score >= 0 else 'negative', 'index': index})
+    result_data = {'sentiment': 'positive' if result.document_sentiment.score >= 0 else 'negative'}
+    result_queue.put(result_data)
 
 class GoogleSentimentAnalysis(SentimentAnalysis):
 
@@ -21,23 +18,16 @@ class GoogleSentimentAnalysis(SentimentAnalysis):
         self.MAX_COMMENT_SIZE = 5000
 
     def classify(self, documents):
-        batches = self.chunks(documents, 600)
+        result_queque = queue.Queue()
+        request_queue = RequestQueue(function_to_call=call_google_sentiment,
+                                    iterate_args=documents,
+                                    fixed_args=[self.client, result_queque],
+                                    request_rate_min=600)
+        request_queue.run()
+
         results = []
-        first = True
-        for b in batches:
-            if not first:
-                print('Waiting for 70 seconds due to 600/min maximum requests')
-                sleep(70)
-            q = queue.Queue()
-            threads = [threading.Thread(target=call_google_sentiment, args=(self.client, r, i, q)) for i, r in enumerate(b)]
-            for t in threads:
-                t.daemon = True
-                t.start()
-            ans = []
-            for i in range(len(b)):
-                ans.append(q.get())
-            ans.sort(key=lambda r: r['index'])
-            ans = list(map(lambda r: r['sentiment'], ans))
-            results.extend(ans)
-            first = False
+        for i in range(len(documents)):
+            results.append(result_queque.get())
+        results = list(map(lambda r: r['sentiment'], results))
+        
         return results
