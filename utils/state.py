cd1208385a8061f2f715bc2368ce2eed3763795d
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2021-11-08 20:12:37 -0300

    salva um snapshot do dataset com ruído em cada passo para posterior comparação

--- utils/state.py
+++ utils/state.py
@@ -1,5 +1,18 @@
 import json
+from pathlib import Path
 
+def save_noised_dataset_snapshot(current_provider, algo, noise_level, noised_dataset, root):
+    path = root + '/snapshots/'+current_provider + '/' + algo
+
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    with open(path+'/'+str(noise_level)+'.json', 'w') as f:
+        json.dump({
+            "current_provider": current_provider,
+            "current_algo": algo,
+            "current_noise_level": noise_level,
+            "noised_dataset": noised_dataset
+        }, f)
 
 def update_state(current_provider, algo, noise_level, noised_dataset, results):
     with open('state.json', 'w') as f:
