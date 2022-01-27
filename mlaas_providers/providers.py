8735d9abc5c1cff93bdecab9d3514f740e8f2bbb
Juliano R Barbosa <rocha.juliano.b@gmail.com>
2022-01-27 09:07:18 -0300

    refatoramento v2

--- mlaas_providers/providers.py
+++ mlaas_providers/providers.py
@@ -1,10 +1,15 @@
-from .azure_sentiment_analysis import AzureSentimentAnalysis
+from heapq import nsmallest
 import random
-
-# microsoft
+import sys
+import pandas as pd
+from .azure_sentiment_analysis import AzureSentimentAnalysis
 from .amazon_sentiment_analysis import AmazonSentimentAnalysis
 from .google_sentiment_analysis import GoogleSentimentAnalysis
+from mlaas_providers import providers as ml_providers
+from pathlib import Path
 
+sys.path.append(".")
+from progress import progress_manager
 
 def azure(dataset):
     azure = AzureSentimentAnalysis()
@@ -25,3 +30,67 @@ def naive_classifier(dataset):
 
     # print(result)
     return result
+
+###############################
+def save_data_to_file(data, path, file_name):
+    df = pd.DataFrame(data, columns =['sentiment'])
+    file_name = file_name+'.xlsx'
+
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    df.to_excel(path+'/'+file_name, 'data', index=False)
+
+def get_providers_instances(func_names, functions_obj):
+    functions = []
+    for name in func_names:
+        function = getattr(functions_obj, name)
+        functions.append(function)
+    
+    return functions
+
+# get available noises for especified algorithm
+def get_available_noise_levels(noive_levels_progress):
+    noise_levels_filtered = []
+    for n in list(noive_levels_progress.keys()):
+        if(noive_levels_progress[str(n)] is None):
+            noise_levels_filtered.append(n)
+
+    noise_levels_filtered = [float(l) for l in noise_levels_filtered]
+
+    return noise_levels_filtered
+
+def get_prediction_results(main_path):
+    progress = progress_manager.load_progress(main_path)
+
+    providers_input = list(progress['predictions'].keys())
+    providers = get_providers_instances(providers_input, ml_providers)
+
+    # predictions = progress["predictions"] # falta salvar o estado
+    noise_data = progress['noise']
+    for provider_algo in providers:
+        print('-',provider_algo.__name__)
+        noise_algorithms = list(progress["predictions"][provider_algo.__name__].keys())
+        for noise in noise_algorithms:
+            print('--', noise)
+            noise_levels_data = list(noise_data[noise].keys())
+            ## buscar aqui
+            noise_levels_available = progress["predictions"][provider_algo.__name__][noise]
+            noise_levels_available = get_available_noise_levels(noise_levels_available)
+            for nlevel in noise_levels_available:
+                print('---', nlevel)
+                dataset_path = noise_data[noise][str(nlevel)]
+
+                dataset = pd.read_excel(dataset_path)
+                dataset_list = dataset.values.tolist()
+                dataset_list = [line[0] for line in dataset_list]
+
+                predicted = provider_algo(dataset_list)
+
+                path = main_path + '/predictions/' + provider_algo.__name__ + '/' + noise
+                file_name = 'predictions-'+str(nlevel)
+                save_data_to_file(predicted, path, file_name)
+
+                progress["predictions"][provider_algo.__name__][noise][str(nlevel)]=path+'/'+file_name+".xlsx"
+                progress_manager.save_progress(main_path, progress)
+
+    return progress
--- noise_insertion/noise_insertion.py
+++ noise_insertion/noise_insertion.py
@@ -1,112 +1,81 @@
+from noise_insertion import noises
+from pathlib import Path
+import pandas as pd
+import sys
+sys.path.append(".")
+from progress import progress_manager
+import inspect
+import argparse
 
-import nlpaug.augmenter.char as nac
-import nlpaug.augmenter.word as naw
-import nlpaug.augmenter.sentence as nas
-import nlpaug.flow as nafc
-import string
-import random
-from random import randrange
-from nlpaug.util import Action
-import os
+'''
+1. Gerar o ruídos:
+   .Entrada = Dataset inteiro
+   .Saída = dataset.csv amostragem do dataset, dataset-0.X.csv para cada noise aplicado
+    --Timestamp_sizeX
+    ----data
+    ------original
+    --------dataset.csv
+    ------ocr
+    --------dataset-0.1.csv, dataset-0.2.csv,...
+'''
 
-from noise_insertion import augmentation as aug
+def generate_noised_dataset(x, noise_level, noise_func):
+    x_noised = noise_func(x,aug_level=noise_level)
 
-def tokenizer(text):
-    return [text]
+    return x_noised
 
-def reverse_tokenizer(token_list):
-    return ''.join(token_list).strip()
+def save_data_to_file(data, path, file_name):
+    df = pd.DataFrame(data, columns =['review'])
+    file_name = file_name+'.xlsx'
 
-def keyboard_aug(text_lists,aug_level=0.3):
-    augmented_texts = []
+    Path(path).mkdir(parents=True, exist_ok=True)
 
-    # aug = nac.KeyboardAug(aug_char_p=aug_level, aug_word_max=None, aug_word_p=1)
-    aug = nac.KeyboardAug(aug_char_p=aug_level,
-                      aug_char_max=None,
-                      tokenizer = tokenizer,
-                      reverse_tokenizer=reverse_tokenizer)
+    df.to_excel(path+'/'+file_name, 'data', index=False)
 
-    for text in text_lists:
-        size = len(text)
-
-        augmented_text = aug.augment(text, n=1)
-        augmented_texts.append(augmented_text)
+def get_noise_instances(func_names, functions_obj):
+    functions = []
+    for name in func_names:
+        function = getattr(functions_obj, name)
+        functions.append(function)
     
-    # print(augmented_texts)
-    return augmented_texts
+    return functions
 
+# get available noises for especified algorithm
+def get_noise_levels(progress, algorithm):
+    noise_levels = list(progress['noise'][algorithm].keys())
 
-def ocr_aug(text_lists, aug_level=0.3):
-    aug = nac.OcrAug(aug_char_p=aug_level,
-                    aug_char_max=None,
-                    aug_word_max=None,
-                    tokenizer = tokenizer,
-                    reverse_tokenizer=reverse_tokenizer)
+    noise_levels_filtered = []
+    for n in noise_levels:
+        if(progress['noise'][algorithm][n] is None):
+            noise_levels_filtered.append(n)
 
-    augmented_texts = []
+    noise_levels_filtered = [float(l) for l in noise_levels_filtered]
 
-    for text in text_lists:
-        augmented_text = aug.augment(text, n=1)
-        augmented_texts.append(augmented_text)
+    return noise_levels_filtered
 
-        # return_similarity(text, augmented_text)
-    
-    # print(augmented_texts)
-    return augmented_texts
+def generate_noised_data(x_dataset, main_path):
+    progress = progress_manager.load_progress(main_path)
+    noise_algorithms_input = list(progress['noise'].keys())
+    noise_algorithms = get_noise_instances(noise_algorithms_input, noises)
 
-def random_noise(text_lists,aug_level=0.3):
-    aug = nac.RandomCharAug(aug_char_p=aug_level,
-                    aug_char_max=None,
-                    tokenizer = tokenizer,
-                    reverse_tokenizer=reverse_tokenizer,
-                    spec_char='!@#$%^&*()_+.' #checar se inclui o ponto
-                    )
+    save_data_to_file(x_dataset, main_path + '/data', "dataset")
+    for j in range(0, len(noise_algorithms)):
+        algorithm = noise_algorithms[j]
+        algorithm_name = noise_algorithms[j].__name__
+        print('-',algorithm_name)
+        noise_levels = get_noise_levels(progress, algorithm_name)
 
-    augmented_texts = []
+        for k in range(0, len(noise_levels)):
+            level = noise_levels[k]
+            print('--', level)
+            if noise_algorithms[j] == noises.no_noise:
+                level = 0
+            noised_dataset = generate_noised_dataset(x_dataset, level, algorithm)
 
-    for text in text_lists:
-        augmented_text = aug.augment(text, n=1)
-        augmented_texts.append(augmented_text)
+            path = main_path + '/data/' + algorithm_name
+            file_name = 'dataset-'+str(level)
+            save_data_to_file(noised_dataset, path, file_name)
 
-    return augmented_texts
-
-def char_swap_noise(text_lists,aug_level=0.3):
-    """
-    Noise insertion by swapping characters.
-
-    Notes:
-        - noise level grows up in even number increments because in each swap two characters are changed
-        - it seems that in some situations it repeats the swap in the same characters, making them go back to what they were before and decreasing the noise level 
-    :param text_lists: list of texts to generate noise in
-    :aug_level: noise level
-    """
-    aug = nac.RandomCharAug(action='swap',
-                    swap_mode='adjacent', # adjacent, middle or random
-                    aug_char_p=aug_level/2, # needs to be divided because nlpaug interpretation of augmentation unit
-                    aug_char_max=None,
-                    tokenizer = tokenizer,
-                    reverse_tokenizer= reverse_tokenizer)
-
-    augmented_texts = []
-
-    for text in text_lists:
-        augmented_text = aug.augment(text, n=1)
-        augmented_texts.append(augmented_text)
-
-    return augmented_texts
-
-def no_noise(text_lists, aug_level=0):
-    return text_lists
-
-def return_similarity(a,b):
-    size = len(a) if len(a) > len(b) else len(b)
-    a = a.ljust(size)
-    b = b.ljust(size)
-    # print(len(a), '-', len(b), '=', size)
-    equals = 0
-    for i in range(size):
-        if(a[i]==b[i]):
-            equals+=1
-    
-    return {"equals": equals, "diff": size-equals,"similarity": equals/size}
-
+            progress["noise"][algorithm_name][str(level)]=path+'/'+file_name+".xlsx"
+            progress_manager.save_progress(main_path, progress)
+    return progress
